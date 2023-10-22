20ba8f57c4c703dd7cd38707febe5a8471717828
Nicolas Castro <nicolas.castro@facens.br>
2023-10-22 15:38:24 -0300

    Update frigo.lua

--- data/spells/scripts/attack/frigo.lua
+++ data/spells/scripts/attack/frigo.lua
@@ -1,3 +1,11 @@
+
+--I tried lots of other ways to replicate de effect of the video, like create a new effect using ObjectBuilder but
+--this i will take ages to replicate and i will only show my art skills on AsePrite and not coding skills,
+--but going this way make me learn how the client load assets, another way i tried is 
+--create combat areas randonly for every time the player cast, and took some time to me to realise,
+--combat areas are only loadded in the start of the server and you can not change inside onCastSpell().
+--So the code below is the closiest i can get to replicate the spell.
+
 --To make that spell i started duplicating the Ethernal_Winter and adding the spell to spells.xml
 --Another change i have to make is, add some code to lib/core/combat.lua so i can call onGetFormulaValues multiple times
 --follwing that solution https://otland.net/threads/tfs-1-x-combat-setcallbackfunction-event-function.283490/
@@ -40,15 +48,14 @@ local combatAreas = {
 	}
 }
 
---Setup the damage formula
+--Creating the damage formula
 function onGetFormulaValues(player, level, magicLevel)
 	local min = (level / 5) + (magicLevel * 5.5) + 25
 	local max = (level / 5) + (magicLevel * 11) + 50
 	return -min, -max
 end
 
-
---Creating the combat array to cast multiple waves with diferent combatAreas
+--Creating a combat array to cast multiple waves with diferent combatAreas
 --I made this way because i want to make easier to change the number of waves, 
 --you just have to add or remove a new matrix to combatAreas
 
